c1a327071e8b6e8383f7984602f181ead4069b14
Diesun7 <127696733+Diesun7@users.noreply.github.com>
2025-06-05 01:28:48 +0300

    Add files via upload

---
+++ deepseek_bash_20250604_63c3a1.sh
@@ -0,0 +1,17 @@
+# Перейдіть у папку з вашим проектом
+cd шлях_до_папки_з_проектом
+
+# Ініціалізуйте Git репозиторій
+git init
+
+# Додайте файли
+git add .
+
+# Зробіть коміт
+git commit -m "Initial commit with website files"
+
+# Додайте віддалений репозиторій (замініть yourusername на ваш GitHub логін)
+git remote add origin https://github.com/yourusername/kylv-luxeboxquets.git
+
+# Завантажте зміни на GitHub
+git push -u origin master
